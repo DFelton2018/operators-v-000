8f3c3cfb1758d5fd278f88734d94d36e16737433
Duane Felton <dfelton2010@gmail.com>
2018-09-13 14:24:44 +0000

    automatically backed up by learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -8,11 +8,7 @@ end
 
 
 def not_safe?(speed)
-<<<<<<< HEAD
 	 speed > 60 || speed < 40 ? true : false
-=======
-	(speed > 60 || speed < 40) ? true : true
->>>>>>> b4ab5763e96c0dfa5fb4e1c8e46e6ff4518c14ed
 end
 
 
